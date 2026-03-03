da4882fb0572d320265388c68dc13c3ca9224bb4
Muhammad Muizz Sajid <muizzsajid@gmail.com>
2026-03-03 19:02:48 +0500

    Add files via upload

---
+++ Test_1/test_1.ino
@@ -0,0 +1,30 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //red
+  for(int i = 0; i<3; i++){
+    digitalWrite(13, HIGH);
+    delay(50);
+    digitalWrite(13, LOW);
+    delay(50);
+  }
+  
+
+  //blue
+  digitalWrite(12, HIGH);
+  delay(500);
+  digitalWrite(12, LOW);
+  delay(500);
+
+  //green
+  digitalWrite(11, HIGH);
+  delay(500);
+  digitalWrite(11, LOW);
+  delay(500);
+}
